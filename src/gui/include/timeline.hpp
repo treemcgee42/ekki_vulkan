fde0bc35f2f7050df08ac0e7c88dbeea4faae111
treemcgee42 <varun.malladi@gmail.com>
2022-09-14 00:43:46 -0400

    added zoom functionality to timelineD

--- src/gui/include/timeline.hpp
+++ src/gui/include/timeline.hpp
@@ -23,13 +23,20 @@ public:
 private:
     float time_at_beginning;
     float time_at_end;
-    float distance_between_seconds;
+    float distance_between_major_ticks;
     ImVec2 canvas_tl_corner;
     ImVec2 canvas_size;
+    ImVec2 canvas_br_corner;
     ImDrawList* drawList;
     float tick_width = 2;
     float second_tick_length = 15;
     ImU32 tick_color = IM_COL32(0, 255, 0, 255);
+    ImU32 ruler_body_color = IM_COL32(255, 0, 0, 255);
+    float ruler_height = 20;
+    // the seconds between major ticks is obtained by raising 2 to this power
+    int ruler_zoom = -1;
+    int ruler_zoom_min = -1;
+    int ruler_zoom_max = 6;
 
     void draw_ruler(eklib::Scene& scene);
 };
--- src/gui/timeline.cpp
+++ src/gui/timeline.cpp
@@ -12,7 +12,7 @@
 namespace ekgui {
 
 Timeline::Timeline(TimelineCreateInfo& create_info):
-    distance_between_seconds{create_info.distance_between_seconds}
+    distance_between_major_ticks{create_info.distance_between_seconds}
 {}
 
 void Timeline::draw(eklib::Scene& scene) {
@@ -22,13 +22,13 @@ void Timeline::draw(eklib::Scene& scene) {
     drawList = ImGui::GetWindowDrawList();
     canvas_tl_corner = ImGui::GetCursorScreenPos();            // ImDrawList API uses screen coordinates!
     canvas_size = ImGui::GetContentRegionAvail();        // Resize canvas to what's available
+    canvas_br_corner = canvas_tl_corner + canvas_size;
     const ImU32 canvasColor = IM_COL32(32, 32, 32, 255);
-
 
     drawList->AddRectFilled(canvas_tl_corner, canvas_tl_corner + canvas_size, canvasColor);
     ImGui::BeginChild("timeline", ImVec2(0, 0), true, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_AlwaysHorizontalScrollbar);
 
-    ImGui::InvisibleButton("dummy", {(1 + scene.get_duration()) * distance_between_seconds, 1});
+    ImGui::InvisibleButton("dummy", {(1 + scene.get_duration()) * distance_between_major_ticks * static_cast<float>(pow(2, -1 * ruler_zoom)), 1});
 
 //    float scene_time = scene.get_current_time();
 //    ImGui::InputFloat("scene time", &scene_time, 0.25f);
@@ -45,30 +45,35 @@ void Timeline::draw(eklib::Scene& scene) {
 }
 
 void Timeline::draw_ruler(eklib::Scene& scene) {
-    auto distance_between_ticks = distance_between_seconds / 4;
+    // ===== Determine major tick scale
+    const auto zoom = std::min(std::max(ruler_zoom, ruler_zoom_min), ruler_zoom_max);
+    // lossless cast since working with powers of 2
+    const auto seconds_between_major_ticks = static_cast<float>(pow(2, zoom));
+    const auto seconds_per_tick = seconds_between_major_ticks / 4;
+    const auto distance_between_ticks = distance_between_major_ticks / 4;
 
-    // body of the ruler
-    auto ruler_start_position = canvas_tl_corner;
-    auto ruler_end_position = ruler_start_position + ImVec2(canvas_size.x, 20);
-    drawList->AddRectFilled(ruler_start_position, ruler_end_position, IM_COL32(255, 0, 0, 255));
+    // ===== Body of the ruler
+    const auto ruler_start_position = canvas_tl_corner;
+    const auto ruler_end_position = ruler_start_position + ImVec2(canvas_size.x, ruler_height);
+    drawList->AddRectFilled(ruler_start_position, ruler_end_position, ruler_body_color);
 
-    // find the first visible tick
-    // ScrollX() will tell us what pixel of the canvas is the at the beginning. There is a
-    // second tick at every `distance_between_ticks` pixels.
-    auto scroll_x = ImGui::GetScrollX();
-    auto scroll_x_tick = scroll_x / distance_between_ticks;
-    auto first_tick = std::ceil(scroll_x_tick);
-    auto first_tick_x = first_tick * distance_between_ticks - scroll_x;
-    int tick_type = static_cast<int>(first_tick) % 4;
+    // ===== Ruler ticks
+    const auto scroll_x = ImGui::GetScrollX();
+    const auto scroll_x_tick = scroll_x / distance_between_ticks;
+    const auto first_tick = std::ceil(scroll_x_tick);
+    const auto first_tick_x = first_tick * distance_between_ticks - scroll_x;
 
-    // determine how many ticks we need to draw (currently assuming quarter second subdivisions)
-    auto number_of_ticks = std::floor((ruler_end_position.x - (ruler_start_position.x + tick_width + first_tick_x)) / distance_between_ticks) + 1;
+    const auto number_of_ticks = std::floor((ruler_end_position.x - (ruler_start_position.x + tick_width + first_tick_x)) / distance_between_ticks) + 1;
 
-    // draw the ticks
+    // Regardless of zoom level, there are 4 divisions between major ticks.
+    // 0 - major tick
+    // 1/3 - small tick
+    // 2 - medium tick
+    // This mod 4 representation correctly describes the order in which these ticks are displayed.
+    int tick_type = static_cast<int>(first_tick) % 4; // safe to cast since this is a ceiling value
     float i = 0;
-    float tick_time = static_cast<int>(first_tick) * 0.25;
+    float tick_time = first_tick * seconds_per_tick;
     while (i < number_of_ticks) {
-        // seeing if this is a second, half-second, or quarter-second
         float this_tick_length = second_tick_length;
         switch (tick_type) {
             case 0:
@@ -84,33 +89,28 @@ void Timeline::draw_ruler(eklib::Scene& scene) {
                 assert(false);
         };
 
-        auto tick_start_position = ImVec2(ruler_start_position.x + first_tick_x + i * distance_between_ticks, ruler_start_position.y);
-        auto tick_end_position = tick_start_position + ImVec2(tick_width, this_tick_length);
-
+        const auto tick_start_position = ImVec2(ruler_start_position.x + first_tick_x + i * distance_between_ticks, ruler_start_position.y);
+        const auto tick_end_position = tick_start_position + ImVec2(tick_width, this_tick_length);
         drawList->AddRectFilled(tick_start_position, tick_end_position, tick_color);
 
         if (tick_type == 0) {
             char time_string[10];
-            sprintf(time_string, "%d", static_cast<int>(tick_time));
+            sprintf(time_string, "%g", tick_time);
             drawList->AddText(tick_start_position + ImVec2(0.5f*distance_between_ticks, 0.5f*(tick_end_position.y - tick_start_position.y)), tick_color, time_string);
-
         }
 
         i += 1;
-        tick_time += 0.25;
+        tick_time += seconds_per_tick;
         tick_type = (tick_type + 1) % 4;
     }
 
-    // draw current time marker
-    // 1. check if that time is currently visible on the timeline. only continue if it is.
-    // 2. determine the time the first tick represents, and its offset
-    // 3. draw at (ticks between first tick and current time) * distance between ticks + first tick offset
-    auto ct = scene.get_current_time();
-    float current_time_tick = ct * 4;
-    auto ct_tick_p1 = ruler_start_position.x + (current_time_tick - scroll_x_tick) * distance_between_ticks;
+    // ===== Draw current time marker
+    const auto ct = scene.get_current_time();
+    const float current_time_tick = ct / seconds_per_tick;
 
-    auto ct_tick_start = ImVec2(ct_tick_p1, ruler_start_position.y);
-    auto ct_tick_end = ImVec2(ct_tick_p1 + tick_width, (canvas_tl_corner + canvas_size).y);
+    const auto ct_tick_p1 = ruler_start_position.x + (current_time_tick - scroll_x_tick) * distance_between_ticks;
+    const auto ct_tick_start = ImVec2(ct_tick_p1, ruler_start_position.y);
+    const auto ct_tick_end = ImVec2(ct_tick_p1 + tick_width, (canvas_tl_corner + canvas_size).y);
 
     drawList->AddRectFilled(
         ct_tick_start,
@@ -118,15 +118,15 @@ void Timeline::draw_ruler(eklib::Scene& scene) {
         IM_COL32(0, 0, 255, 255)
     );
 
-    // adjust scene time based on a click
+    // adjust based on a left click
+    const auto mouse_pos = ImGui::GetMousePos();
     if (ImGui::IsMouseClicked(0)) {
-
-
-        // get time of where clicked and adjust scene time
-        auto mouse_x = ImGui::GetMousePos().x - canvas_tl_corner.x;
-        auto time = ((mouse_x - first_tick_x) / distance_between_ticks) * 0.25 + first_tick / 4;
-        //std::cout << "clicked at " << mouse_x << ", time: " << time << "\n";
-        scene.adjust_current_time(time);
+        if (mouse_pos.x > canvas_tl_corner.x && mouse_pos.x < canvas_br_corner.x && mouse_pos.y > canvas_tl_corner.y && mouse_pos.y < canvas_br_corner.y) {
+            const auto mouse_x = ImGui::GetMousePos().x - canvas_tl_corner.x;
+            const auto time = ((mouse_x - first_tick_x) / distance_between_ticks) * seconds_per_tick +
+                              first_tick * seconds_per_tick;
+            scene.adjust_current_time(time);
+        }
     }
 }
 
